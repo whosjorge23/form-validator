71ae6b44202ebb48c49ebae47fb2775cb06e82de
Giorgio Giannotta <47144634+whosjorge23@users.noreply.github.com>
2020-09-09 12:25:20 +0200

    Check Validation Password, Add Credit

--- script.js
+++ script.js
@@ -5,22 +5,61 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
-    //Using Contraint API
+    // Use HTML constraint API to check form validity
     isValid = form.checkValidity()
-    console.log(isValid)
     // Style Main Message For An Error
-    message.textContent = 'Please fill out all fields'
-    message.style.color = '#E74C3D'
-    messageContainer.style.borderColor = '#E74C3D'
+    if (!isValid) {
+        message.textContent = 'Please fill out all fields.'
+        message.style.color = '#E74C3D'
+        messageContainer.style.borderColor = '#E74C3D'
+        return
+    }
+    //Check to see if Password match
+    if (password1Element.value === password2Element.value) {
+        passwordsMatch = true;
+        password1Element.style.borderColor = '#2FCC71'
+        password2Element.style.borderColor = '#2FCC71'
+    }else {
+        passwordsMatch = false
+        message.textContent = 'Make sure passwords match.'
+        message.style.color = '#E74C3D'
+        messageContainer.style.borderColor = '#E74C3D'
+        password1Element.style.borderColor = '#E74C3D'
+        password2Element.style.borderColor = '#E74C3D'
+        return
+    }
+    //If form is valid and password match
+    if(isValid && passwordsMatch) {
+        message.textContent = 'Successfully Registered!'
+        message.style.color = '#2FCC71'
+        messageContainer.style.borderColor = '#2FCC71'
+    }
+}
+
+function storeFormData() {
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value,
+      };
+    // Do Something with user data
+    console.log(user)
 }
 
 function processFormData(e) {
     e.preventDefault();
-    console.log(e)
+    //console.log(e)
     //Validate Form
     validateForm()
+    //Submit Data if Valid
+    if(isValid && passwordsMatch) {
+        storeFormData()
+    }
 }
 
 //Event Listener
